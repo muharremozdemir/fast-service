9bd8c207e4ac3d20917e24f069905c230c2513ee
muharremozdemir <ozdemir.muharrem34@gmail.com>
2026-03-15 14:03:36 +0300

    Fix

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -55,7 +55,7 @@ class OrderController extends Controller
                 'notes' => $order->notes,
                 'created_at' => $order->created_at->format('Y-m-d H:i:s'),
                 'items' => $items->map(function ($item) {
-                    dd($item->product->getTranslation("name", "tr"));
+                    dd($item->product->getTranslation("name", app()->getLocale()));
                     return [
                         'id' => $item->id,
                         'product' => [
